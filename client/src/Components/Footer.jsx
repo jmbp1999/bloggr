20fe8e9eba8cd9b21d857a08f0362d75634acc14
jmbp1999 <pamudithaj98@gmail.com>
2022-10-02 16:35:48 +0530

    initialize components

---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src='' alt='Logo' />
+      <span>blogger</span>
+    </footer>
+  );
+};
+
+export default Footer;
