6443236f2e0bc2b5cebcc0df0a3d8c08d99b3949
jmbp1999 <pamudithaj98@gmail.com>
2022-10-02 16:33:25 +0530

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,59 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider, Route, Outlet } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Post from './Pages/Post';
+import Register from './Pages/Register';
+import Write from './Pages/Write';
+import './styles.scss';
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/write',
+        element: <Write />,
+      },
+      {
+        path: '/post:id',
+        element: <Post />,
+      },
+    ],
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+]);
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className='app'>
+      <div className='container'>
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
