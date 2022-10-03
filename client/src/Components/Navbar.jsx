54960b8a20b39a5b141913081f43b0caec084140
jmbp1999 <pamudithaj98@gmail.com>
2022-10-03 18:40:13 +0530

    logout functionality

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/authContext';
+
 const Navbar = () => {
+  const { currentUser, logout } = useContext(AuthContext);
+
   return (
     <div className='navbar'>
       <div className='container'>
         <div className='logo'>
-          <img className='logo' src='' alt='Logo' />
+          <Link to='/'>
+            <img src='' alt='Logo' />
+          </Link>
         </div>
         <div className='links'>
-          {' '}
           <Link className='link' to='/?cat=art'>
             <h6>ART</h6>
           </Link>
@@ -27,9 +32,9 @@ const Navbar = () => {
           <Link className='link' to='/?cat=food'>
             <h6>FOOD</h6>
           </Link>
-          <span>Pamuditha</span>
-          {true ? (
-            <span>Logout</span>
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
           ) : (
             <Link className='link' to='/login'>
               Login
