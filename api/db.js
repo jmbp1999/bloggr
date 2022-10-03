26f2e5f1e5330403fff51f28535a0ba897db248a
jmbp1999 <pamudithaj98@gmail.com>
2022-10-03 18:41:20 +0530

    relavant packages and refactor startup

--- api/db.js
+++ api/db.js
@@ -1,10 +1,10 @@
-import mysql from 'mysql';
+import mysql from 'mysql2';
 import dotenv from 'dotenv';
 
 dotenv.config();
 export const db = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'blog',
 });
--- api/index.js
+++ api/index.js
@@ -2,20 +2,24 @@ import express from 'express';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import postRoutes from './routes/posts.js';
-import mysql from 'mysql';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import { db } from './db.js';
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cookieParser());
+
+app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 
 db.connect(
-  (data) => {
+  () => {
     app.listen(8800, () => {
-      console.log('Connected!');
-      console.log(data);
+      console.log('Server is up and running on PORT 8800');
     });
   },
   (err) => {
